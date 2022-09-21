b5780f8a023e6ee4eadc0f62148465b13750ba1e
Villanueva <e5p10@DESKTOP-SDK43SM.localdomain>
2022-09-21 08:18:32 -0500

    update: argc

--- Evaluation/cache__matrix_multiplication__blocks.cpp
+++ Evaluation/cache__matrix_multiplication__blocks.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include "../helpers/matrix/matrix_2d_pointer_generator.hpp"
+#include "../helpers/my_classes/timer.hpp"
 
 int **A, **B, **C;
 
-int main(){
-    int N;
-    std::cin>>N;    
+int main(int argc, char**argv){
+    int N=std::atoi(argv[1]);
+    Timer timer01; 
+    int blocksize=N/2;
 
     matrix_2d_ptr__create_and_fill__random_values(A,N,N,1,5);
     matrix_2d_ptr__create_and_fill__random_values(B,N,N,1,5);
     matrix_2d_ptr__create_and_fill__0s(C,N,N);
-
-    int blocksize=N/2;
+    
+    timer01.start();
     for (int ii = 0; ii < N; ii+=blocksize)
         for (int jj = 0; jj < N; jj+=blocksize)
             for (int kk = 0; kk < N; kk+=blocksize)
@@ -19,6 +21,5 @@ int main(){
                     for (int j = jj; j < jj+blocksize; j++)
                         for (int k = kk; k < kk+blocksize; k++)
                             *(*(C+i)+j) += *(*(A+i)+k) * *(*(B+k)+j);
-
-    matrix_2d_ptr__print(C,N,N);
+    timer01.stop();
 }
--- Evaluation/cache__matrix_multiplication__clasic.cpp
+++ Evaluation/cache__matrix_multiplication__clasic.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include "../helpers/matrix/matrix_2d_pointer_generator.hpp"
+#include "../helpers/my_classes/timer.hpp" 
 
 int **A, **B, **C;
 
-int main(){
-    int N;
-    std::cin>>N;
+int main(int argc, char**argv){
+    int N=std::atoi(argv[1]);
+    Timer timer01;
 
     matrix_2d_ptr__create_and_fill__random_values(A,N,N,1,5);
     matrix_2d_ptr__create_and_fill__random_values(B,N,N,1,5);
     matrix_2d_ptr__create_and_fill__0s(C,N,N);
 
+    timer01.start();
     for(int i = 0; i < N; ++i)
         for(int j = 0; j < N; ++j)
             for(int k = 0; k < N; ++k)
                 *(*(C+i)+j) += *(*(A+i)+k) * *(*(B+k)+j);
-    
-    matrix_2d_ptr__print(C,N,N);
+    timer01.stop();
 }
